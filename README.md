fea7bba6fa28530f5da3cc1dbb200476754c0c02
Derk <derky@phpbb.com>
2026-03-01 16:04:04 +0100

    Add base extension files

---
+++ README.md
@@ -0,0 +1,13 @@
+# AI & Scraper Shield
+
+phpBB extension
+
+## Installation
+
+1. Copy the extension to: `phpBB/ext/derky/aiscrapershield`
+2. In the ACP, go to: **Customise → Manage extensions**
+3. Enable the **AI & Scraper Shield** extension
+
+## License
+
+Licensed under the [GNU General Public License v2](license.txt)
---
+++ ext.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ *
+ * AI & Scraper Shield. An extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2026, Derky
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+namespace derky\aiscrapershield;
+
+/**
+ * AI & Scraper Shield Extension base
+ *
+ * It is recommended to remove this file from
+ * an extension if it is not going to be used.
+ */
+class ext extends \phpbb\extension\base
+{
+}
